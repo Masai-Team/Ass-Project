9c718639c76e76fe58937f5872c85ed8858ff701
spannase <shreya16pannase@gmail.com>
2021-12-18 15:29:14 +0530

    our brands

---
+++ OurBrands.js
@@ -0,0 +1,21 @@
+
+document.querySelector("#Rfresh").addEventListener("click",function(){
+    window.location.href="relianceFresh.html"
+});
+document.querySelector("#Rsmart").addEventListener("click",function(){
+    window.location.href="relianceSmart.html"
+});
+document.querySelector("#jioMart").addEventListener("click",function(){
+    window.location.href="jioMart.html"
+});
+document.querySelector("#Rdigital").addEventListener("click",function(){
+    window.location.href="relianceDigital.html"
+});
+document.querySelector("#Footwear").addEventListener("click",function(){
+    window.location.href="trendsFootwear.html"
+});
+document.querySelector("#ajio").addEventListener("click",function(){
+    window.location.href="ajio.html"
+});
+
+
